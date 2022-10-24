54756c65efbebda62a2545672fef414de3d39d47
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-10-24 16:36:44 -0500

    add Player to view page

--- pages/view.tsx
+++ pages/view.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Player } from '@livepeer/react'
+import styles from '../styles/Home.module.css';
 
 export default function view() {
   return (
-    <div>view</div>
-  )
+    <div className={ styles.main }>
+      <h1>Enjoy the video</h1>
+      <div className={styles.card}>
+        <Player
+          // playbackId={ }
+          showPipButton
+
+        />
+      </div>
+    </div>
+  );
 }
