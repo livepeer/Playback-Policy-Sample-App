2e78a516971a8136f80b02d970a73e5fc0d4f62e
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-10-24 16:31:33 -0500

    remove footer styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { LivepeerConfig, createReactClient, studioProvider } from '@livepeer/react';
+import { ConnectKitProvider, getDefaultClient } from 'connectkit';
+import { WagmiConfig, chain, createClient } from 'wagmi';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp( { Component, pageProps }: AppProps ) {
+  
+   const wagmiClient = createClient(
+     getDefaultClient({
+       appName: 'Playback Policy',
+       chains: [chain.goerli],
+       infuraId: process.env.NEXT_PUBLIC_INFURA_API_KEY,
+     })
+   );
+  
+   const client = createReactClient({
+     provider: studioProvider({ apiKey: process.env.NEXT_PUBLIC_API_CORS }),
+   });
+  
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <ConnectKitProvider>
+          <LivepeerConfig client={client}>
+            <Component {...pageProps} />
+          </LivepeerConfig>
+        </ConnectKitProvider>
+      </WagmiConfig>
+    </>
+  );
 }
 
 export default MyApp
