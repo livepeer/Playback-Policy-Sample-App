9420b65633a172d46e86002fa31ad706eabbf17a
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-10-24 20:36:36 -0500

    add api route for applying playback policy to stream

---
+++ pages/api/applyPlaybackPolicy.ts
@@ -0,0 +1,22 @@
+export default async function handler( req: any, res: any ) {
+  const {streamId} = req.body
+  try {
+    const response = await fetch(`https://livepeer.studio/api/stream/${streamId}`, {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${process.env.STAGING_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify( {
+        'playbackPolicy': {
+          'type': 'jwt'
+        }
+      })
+    });
+
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
