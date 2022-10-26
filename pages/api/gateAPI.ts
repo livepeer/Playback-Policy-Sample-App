4ada2034a2a19c6a9f9d5e8f89fa28395d381f38
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-10-26 13:31:00 -0500

    add

--- pages/api/gateAPI.ts
+++ pages/api/gateAPI.ts
@@ -1,20 +1,36 @@
+import jwt, {JsonWebTokenError, JwtPayload, Secret} from 'jsonwebtoken'
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse ) {
-  const { type, pub, stream } = req.body;
-
+  const { playbackId } = req.body;
+  const publicKey = process.env.PUBLIC_KEY;
+  
+  
+  const expiration = Math.floor( Date.now() / 1000 ) + 1000;
+  const payload: JwtPayload = {
+    sub: playbackId,
+    action: 'pull',
+    iss: 'Livepeer Studio',
+    pub: publicKey,
+    exp: expiration,
+    video: 'none'
+  }
+  
+  const token = jwt.sign( payload, process.env.PRIVATE_KEY as Secret, { algorithm: 'ES256' } )
+  
+  
+  // TODO: Change to livepeer.studio when in production
   try {
-    // TODO: Change to livepeer.studio when in production
-    const response = await fetch(`https://livepeer.monster/api/access-control/gate`, {
-      method: 'POST',
-      headers: {
+  const response = await fetch(`https://livepeer.monster/api/access-control/gate`, {
+    method: 'POST',
+    headers: {
         Authorization: `Bearer ${process.env.STAGING_API_KEY}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        type,
-        pub,
-        stream,
+        type: 'jwt',
+        pub:token.Claims.(jwt.MapClaims)["pub"],
+        playbackId,
       }),
     });
 
