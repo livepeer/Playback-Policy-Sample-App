4e926e65b3be993be82adcfff1f422aea41a775f
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-10-26 21:31:41 -0500

    add get key info by id feature

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -8,8 +8,10 @@ export default function CreateStream() {
   const [ playbackId, setPlaybackId ] = useState<string>();
   const [ streamId, setStreamId ] = useState<string>();
   const [ keyId, setKeyID ] = useState<string | null>(null);
+  const [ deleteKeyId, setDeleteKeyId ] = useState<string>();
   const [ getKeyId, setGetKeyId ] = useState<string>();
   const [ keyName, setKeyName ] = useState<string>();
+  const [ signKeyInfo, setSignKeyInfo ] = useState<object>();
   const [ userId, setUserId] = useState<string>();
   const [ createdAt, setCreatedAt ] = useState<string>();
   const [ publicKey, setPublicKey ] = useState<string>();
@@ -79,6 +81,22 @@ export default function CreateStream() {
     }
 }
 
+// Get key by Id
+  async function getKeyById( e: FormEvent ) {
+    e.preventDefault();
+    try {
+      const response = await fetch( `/api/keyInfo/${getKeyId}`, {
+        method: 'GET'
+      } )
+      const data = await response.json();
+      setSignKeyInfo( data )
+      console.log(data);
+      
+    } catch (error) {
+      
+    }
+  }
+  
 //Delete Key
 
   async function deleteKey( e: FormEvent ) {
@@ -133,7 +151,7 @@ export default function CreateStream() {
             className={styles.input}
             type='text'
             value={streamId}
-            name='name'
+            name='stream Id'
             required
             onChange={(e) => setStreamId(e.target.value)}
           />
@@ -152,7 +170,7 @@ export default function CreateStream() {
             className={styles.input}
             type='text'
             value={playbackId}
-            name='name'
+            name='Playback Id'
             required
             onChange={(e) => setPlaybackId(e.target.value)}
           />
@@ -162,6 +180,39 @@ export default function CreateStream() {
           </button>
         </form>
 
+        {/* Get key info by Id */}
+        <div className={styles.main2}>
+          <form onSubmit={getKeyById} method='GET' className={styles.card}>
+            <h2>Get Key Info</h2>
+            <label htmlFor='stream'>Key Id: </label>
+            <br />
+            <input
+              className={styles.input}
+              type='search'
+              value={getKeyId}
+              name='query'
+              required
+              onChange={(e) => setGetKeyId(e.target.value)}
+            />
+            <br />
+            <button type='submit' className={styles.button}>
+              Get Key Info
+            </button>
+            {!signKeyInfo ? (
+              <></>
+            ) : (
+              <div className={styles.card}>
+                <h2> Key Info</h2>
+                <p>Id: {signKeyInfo?.id}</p>
+                <p>Name: {signKeyInfo?.name}</p>
+                <p>User Id: {signKeyInfo?.userId}</p>
+                <p>Created At: {signKeyInfo?.createdAt}</p>
+                <p>Public Key: {signKeyInfo?.publicKey}</p>
+              </div>
+            )}
+          </form>
+        </div>
+
         {/* Delete Signing Key */}
         <form onSubmit={deleteKey} method='DELETE' className={styles.card}>
           <h2>Delete Key</h2>
@@ -170,10 +221,10 @@ export default function CreateStream() {
           <input
             className={styles.input}
             type='text'
-            value={getKeyId}
+            value={deleteKeyId}
             name='name'
             required
-            onChange={(e) => setGetKeyId(e.target.value)}
+            onChange={(e) => setDeleteKeyId(e.target.value)}
           />
           <br />
           <button type='submit' className={styles.deletebutton}>
---
+++ pages/api/keyInfo/[signKeyInfo].ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    // TODO: Change to livepeer.studio when in production
+    const response = await fetch(
+      `https://livepeer.monster/api/access-control/signing-key/${req.query.signKeyInfo}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${process.env.STAGING_API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+}
