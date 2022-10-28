36f5e15756cf14b9a1c5a1ef10630382a8119922
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-10-28 14:17:11 -0500

    add check wallet amount for stream access

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,44 +6,44 @@ import styles from '../styles/Home.module.css';
 import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
 
 export default function Login() {
+  // Set minimum amount of Eth in wallet to view(ACL)
   const minimumEth = 0.001;
 
+  // Using Wagmi to get wallet information
+  const { address, isConnected } = useAccount();
+  const { data } = useBalance({
+    addressOrName: address, //Getting wallet address with useAccount()
+    chainId: 5, //Goerli testnet});
+  });
+
   // Creating JWT
+
   const playbackId = 'b5e7kxt3zi69o4x8';
   const expiration = Math.floor(Date.now() / 1000) + 1000;
   const payload: JwtPayload = {
     sub: playbackId,
     action: 'pull',
     iss: 'Livepeer Studio',
-    pub: process.env.PUBLIC_KEY,
+    pub: process.env.NEXT_PUBLIC_PUBLIC_KEY,
     exp: expiration,
     // video: 'none',
   };
 
-  // const token = jwt.sign(payload, process.env.PRIVATE_KEY as Secret, { algorithm: 'ES256' });
-
-  // const playbackURL = `https://livepeercdn.com/hls/${playbackId}/index.m3u8/${token}`;
-
-  // Using Wagmi to get wallet information
-  const { address, isConnected } = useAccount();
-  const { data, isSuccess } = useBalance( {
-    addressOrName: address,
-    chainId: 5, //Goerli testnet});
-  } );
-
-console.log(Number(data?.formatted));
-
   
+  const token = jwt.sign(payload, process.env.NEXT_PUBLIC_PRIVATE_KEY as Secret, {
+    algorithm: 'ES256',
+  });
 
-    
-
+  console.log(token);
+  
+  // const playbackURL = `https://livepeercdn.com/hls/${playbackId}/index.m3u8/${token}`;
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Connect Wallet to view streams</h1>
         <ConnectKitButton />
-        { isConnected && Number(data?.formatted) > minimumEth  && (
+        {isConnected && Number(data?.formatted) > minimumEth && (
           <div className={styles.player}>
             {/* <p>Playback URL: {playbackURL}</p> */}
             <Player playbackId={playbackId} showPipButton loop autoPlay muted />
