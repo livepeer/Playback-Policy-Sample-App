848768e931c8b9e5cb2b84bfeffcc3f54a2cc388
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-10-28 12:34:18 -0500

    show player if wallet has enough funds

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,42 +1,55 @@
 import { ConnectKitButton } from 'connectkit';
+import { useEffect, useState } from 'react';
 import { Player } from '@livepeer/react';
-import { useAccount } from 'wagmi';
+import { useAccount, useBalance } from 'wagmi';
 import styles from '../styles/Home.module.css';
-import jwt, { JwtPayload, Secret} from 'jsonwebtoken'
+import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
 
-  
-  
-  
 export default function Login() {
+  const minimumEth = 0.001;
+
+  // Creating JWT
+  const playbackId = 'b5e7kxt3zi69o4x8';
+  const expiration = Math.floor(Date.now() / 1000) + 1000;
+  const payload: JwtPayload = {
+    sub: playbackId,
+    action: 'pull',
+    iss: 'Livepeer Studio',
+    pub: process.env.PUBLIC_KEY,
+    exp: expiration,
+    // video: 'none',
+  };
+
+  // const token = jwt.sign(payload, process.env.PRIVATE_KEY as Secret, { algorithm: 'ES256' });
+
+  // const playbackURL = `https://livepeercdn.com/hls/${playbackId}/index.m3u8/${token}`;
+
+  // Using Wagmi to get wallet information
+  const { address, isConnected } = useAccount();
+  const { data, isSuccess } = useBalance( {
+    addressOrName: address,
+    chainId: 5, //Goerli testnet});
+  } );
+
+console.log(Number(data?.formatted));
+
   
-const expiration = Math.floor(Date.now() / 1000) + 1000;
-const payload: JwtPayload = {
-  sub: playbackId,
-  action: 'pull',
-  iss: 'Livepeer Studio',
-  pub: publicKey,
-  exp: expiration,
-  video: 'none',
-};
 
-const token = jwt.sign(payload, process.env.PRIVATE_KEY as Secret, { algorithm: 'ES256' });
+    
 
-
-  const { address, isConnected} = useAccount();
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-          <h1 className={styles.title}>Connect Wallet to view streams</h1>
-          <ConnectKitButton />
-          {isConnected && (
-            <div className={styles.player}>
-              <Player playbackId='d93a49mu90eaxweo' showPipButton loop autoPlay muted />
-            </div>
-          )}
+        <h1 className={styles.title}>Connect Wallet to view streams</h1>
+        <ConnectKitButton />
+        { isConnected && Number(data?.formatted) > minimumEth  && (
+          <div className={styles.player}>
+            {/* <p>Playback URL: {playbackURL}</p> */}
+            <Player playbackId={playbackId} showPipButton loop autoPlay muted />
+          </div>
+        )}
       </main>
     </div>
   );
-};
-
-
+}
