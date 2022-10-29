ae9e75ed69cd06a742e2002f91b7ba6d66e6eeaf
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-10-29 16:12:10 -0500

    add logout from siwe

---
+++ pages/api/logout.ts
@@ -0,0 +1,19 @@
+import { ironOptions } from "../../utils";
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async ( req: NextApiRequest, res: NextApiResponse )=> {
+  const { method } = req;
+  switch ( method ) {
+    case 'POST':
+      req.session.destroy();
+      res.setHeader( 'cache-control', 'no-store, max-age=0' );
+      res.send( { ok: true } );
+      break;
+    default:
+      res.setHeader( 'Allow', [ 'Post' ] );
+      res.status( 405 ).end( `Method ${ method } Not Allowed `);
+  }
+}
+
+export default withIronSessionApiRoute( handler, ironOptions );
