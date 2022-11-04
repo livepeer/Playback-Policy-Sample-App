a1c4e31a27cd642b5f0116f9527fc0b04ae6b8ad
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-11-04 13:14:01 -0500

    remove comments and update env

--- pages/api/keyInfo/[signKeyInfo].ts
+++ pages/api/keyInfo/[signKeyInfo].ts
@@ -1,13 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    // TODO: Change to livepeer.studio when in production
     const response = await fetch(
       `https://livepeer.studio/api/access-control/signing-key/${req.query.signKeyInfo}`,
       {
         method: 'GET',
         headers: {
-          Authorization: `Bearer ${process.env.STAGING_API_KEY}`,
+          Authorization: `Bearer ${process.env.API_KEY}`,
           'Content-Type': 'application/json',
         },
       }
